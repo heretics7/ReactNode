03580acde0e6ce9bf8dc60d3ac4555ce477bfa42
heretics7 <heretics7@naver.com>
2022-03-16 16:45:25 +0900

    SQL 연결

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 var express = require('express');
-var app = express();
+var mysql = require('mysql');
+var dbconfig = require('./db/config.js');
+var connection = mysql.createConnection(dbconfig)
 
-var getRoutes = require('./api/getrouter')
 var postRoutes = require('./api/postrouter')
+var getRoutes = require('./api/getrouter')
+
+var app = express();
+app.get('/introduce', (req, res) => {
+    connection.query(
+        'SELECT * FROM introduce_react_node.prev_interview;',
+        (error, result) => {
+            if(error) throw error;
+            res.send(result)
+        }
+    )
+})
 
 app.use('/get', getRoutes )
 app.use('/post', postRoutes )
 
 app.set('port', 8080)
 app.get('/', (req, res) => {
-    res.send(' localhost 첫 페이지 ')
+    res.send(' / 루트요청받음');
 })
 
 app.listen(app.get('port'), () => {
-    console.log(' 이 텍스트가 출력되면 첫 페이지는 정상 작동 중 ')
+    console.log(' 이 텍스트가 출력되면 첫 페이지는 정상 작동 중 ');
 })
