606479c7e32dd91f1a6ed2bd1285f24cdcc733ba
heretics7 <heretics7@naver.com>
2022-03-17 16:05:23 +0900

    노드 리액트 연결

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 var express = require('express');
-var mysql = require('mysql');
-var dbconfig = require('./db/config.js');
-var connection = mysql.createConnection(dbconfig)
+var app = express();
+// var mysql = require('mysql');
+// var dbconfig = require('./db/config.js');
+// var connection = mysql.createConnection(dbconfig)
 
 var postRoutes = require('./api/postrouter')
 var getRoutes = require('./api/getrouter')
+var sqlsend = require('./api/introduce')
 
-var app = express();
-app.get('/introduce', (req, res) => {
-    connection.query(
-        'SELECT * FROM introduce_react_node.prev_interview;',
-        (error, result) => {
-            if(error) throw error;
-            res.send(result)
-        }
-    )
-})
+// app.get('/introduce', (req, res) => {
+//     connection.query(
+//         'SELECT * FROM reactinterview.contact;',
+//         (error, result) => {
+//             if(error) throw error;
+//             res.send(result)
+//         }
+//     )
+// })
 
 app.use('/get', getRoutes )
 app.use('/post', postRoutes )
+app.use('/ontroduce', sqlsend)
 
 app.set('port', 8080)
 app.get('/', (req, res) => {
